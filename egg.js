2c0d0bed9df7cc54528ed0c81cda84c8c3ec0e5c
Christopher Best <bestchris@gmail.com>
2013-08-08 23:14:12 -0400

    randomly place enemies on start

--- egg.js
+++ egg.js
@@ -1,2 +1,16 @@
 var startgame = function() {
+	// set random positions for enemies
+	var enemies = document.getElementsByClassName('enemy');
+	for(var elindex = 0; elindex < enemies.length; elindex++) {
+		enemies[elindex].style.top = randTop().toString() + 'px';
+		console.log(randTop());
+		enemies[elindex].style.left = randLeft().toString() + 'px';
+	}
+};
+
+var randTop = function() {
+	return Math.floor((Math.random()*window.innerHeight) + 1);
+};
+var randLeft = function() {
+	return Math.floor((Math.random()*window.innerWidth) + 1);
 };
